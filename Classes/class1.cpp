e1054d396e66fecd509f3511520595340addaa75
Muzzamal <ashrafmuzzamal7@gmail.com>
2025-01-12 20:37:14 +0500

    Find the area and circumference of a circle using class

--- Classes/class1.cpp
+++ Classes/class1.cpp
@@ -1,6 +1,36 @@
-#include<iostream>
+#include <iostream>
+#define PI 3.14
 using namespace std;
-int main(){
+class circle
+{
+    int rad;
 
-return 0;
+public:
+    void getradius(int rad)
+    {
+        this->rad = rad;
+    }
+    void area()
+    {
+        int area = PI * rad * rad;
+        cout << "Area: " << area << endl;
+    }
+    void circum()
+    {
+        int circum = 2 * PI * rad;
+        cout << "Circumfarance: " << circum << endl;
+    }
+};
+int main()
+{
+    circle c1;
+    int rad;
+    cout<<"Enter the radius: ";
+    cin>>rad;
+    c1.getradius(rad);
+    c1.area();
+    c1.circum();
+
+
+    return 0;
 }
