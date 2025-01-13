127d95e255086a0a6d7cd38693363e4d300da863
Muzzamal <ashrafmuzzamal7@gmail.com>
2025-01-13 20:21:49 +0500

    modified

--- File Handling/task2.cpp
+++ File Handling/task2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     char city[50];
-    ofstream file("G:\\OOP-in-CPP\\Raw\\city.txt");
+    ofstream file("G:\\OOP-in-CPP\\File Handling\\Raw\\city.txt");
     for (int i = 0; i < 5; i++)
     {
         cout << "Enter the name of an city: ";
