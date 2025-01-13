2e8b9cbe268bae1d79e40b037dac54c014263d32
Muzzamal <ashrafmuzzamal7@gmail.com>
2025-01-13 17:48:36 +0500

    enter data in the file

---
+++ File Handling/task2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    char city[50];
+    ofstream file("G:\\OOP-in-CPP\\Raw\\city.txt");
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Enter the name of an city: ";
+        cin >> city;
+        file << city << endl;
+    }
+    file.close();
+
+    return 0;
+}
