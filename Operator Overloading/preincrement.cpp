06eb494199756f725af8eb8201f9f4258f7f7228
Muzzamal <ashrafmuzzamal7@gmail.com>
2025-02-02 17:17:14 +0500

    addition pre increment operator overloading

--- Operator Overloading/preincrement.cpp
+++ Operator Overloading/preincrement.cpp
@@ -1,6 +1,35 @@
 #include<iostream>
 using namespace std;
-int main(){
+class count
+{
+private:
+    int num;
+public:
+    // paramerterized constructor
+    count(int n):num(n){}
 
+    //display the output 
+
+    void display()
+    {
+        cout<<"Num: "<<num<<endl;
+    }
+    
+    //operator overloading ++num
+
+    count operator ++ ()
+    {
+        ++num;
+        return *this;
+    }
+
+};
+int main(){
+    count c(11);
+    c.display();
+    ++c;
+    c.display();
+    ++c;
+    c.display();
 return 0;
 }
