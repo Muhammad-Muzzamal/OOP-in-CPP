dc9ad104365122f5501844e88515dfdfdc4507b2
Muzzamal <ashrafmuzzamal7@gmail.com>
2025-02-02 17:51:48 +0500

    Less than operator overloading

---
+++ Operator Overloading/lessthan.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Test
+{
+private:
+    int num;
+
+public:
+    Test(int n) : num(n) {}
+    void display()
+    {
+        cout << "NUmber: " << num << endl;
+    }
+    bool operator<(Test &obj)
+    {
+        return (num < obj.num);
+    }
+};
+int main()
+{
+    Test t1(11), t2(22);
+    if (t1<t2)
+    {
+        cout<<"Obj1 is less than Obj2";
+    }
+    
+
+    return 0;
+}
