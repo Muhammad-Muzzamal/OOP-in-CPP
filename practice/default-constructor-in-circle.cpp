3fe78cf876f5189bc26dcce2b39fb301f9bbeef2
Muzzamal <ashrafmuzzamal7@gmail.com>
2025-02-03 12:09:48 +0500

    Add default constructor in the circle class to intialize the value of radius

---
+++ practice/default-constructor-in-circle.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#define PI 3.14
+using namespace std;
+class Circle
+{
+private:
+    double radius;
+
+public:
+    Circle() : radius(0.0) {}
+    void setradius(double r)
+    {
+        if (r > 0)
+        {
+            radius = r;
+        }
+        else
+        {
+            cout << "Enter valid number! \n";
+        }
+    }
+    double getRadius()
+    {
+        return radius;
+    }
+    double getArea()
+    {
+        return PI * radius * radius;
+    }
+};
+int main()
+{
+    Circle c;
+    c.setradius(11.5);
+    cout << "Area: " << c.getArea() << endl;
+
+    return 0;
+}
